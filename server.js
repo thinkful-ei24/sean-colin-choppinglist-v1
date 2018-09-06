f5d9a08ab37b15bfff0de29dce62c5a5bb5a3ebb
Colin Rupp <colin@thinkful.com>
2018-09-06 09:19:16 -0700

    added PUT updater

--- server.js
+++ server.js
@@ -65,6 +65,28 @@ app.delete('/recipes/:id', (req, res) => {
 });
 
 
+app.put('/recipes/:id', jsonParser, (req, res) => {
+	const requiredFields = ['name', 'id', 'ingredients'];
+	for (let reqField of requiredFields) {
+		if(!(reqField in req.body)) {
+			const message = `${reqField} needs to be included in the request body`;
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+	if (req.params.id !== req.body.id) {
+		const message = `Request path id ${req.params.id} and request body id ${req.params.id} must match`;
+		console.error(message);
+		return res.status(400).send(message);
+	}
+	Recipes.update({
+		name: req.body.name,
+		id: req.params.id,
+		ingredients: req.body.ingredients
+	});
+	res.status(204).end();
+});
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
